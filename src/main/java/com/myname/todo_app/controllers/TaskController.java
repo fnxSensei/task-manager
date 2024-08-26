3109e4b98ca9141a21a43e450b60e2afd7be1e86
fnx <fenix93.08@mail.ru>
2024-08-26 22:32:43 +0300

    Swager

--- src/main/java/com/myname/todo_app/controllers/TaskController.java
+++ src/main/java/com/myname/todo_app/controllers/TaskController.java
@@ -4,6 +4,8 @@ package com.myname.todo_app.controllers;
 import com.myname.todo_app.exception.TaskNotFoundException;
 import com.myname.todo_app.model.Task;
 import com.myname.todo_app.service.TaskService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -17,6 +19,7 @@ import java.util.Locale;
 
 @RestController
 @RequestMapping("/api/tasks")
+@Api(value = "Task Management System", description = "Operations pertaining to task management in the application")
 public class TaskController {
 
     @Autowired
@@ -25,11 +28,13 @@ public class TaskController {
     @Autowired
     private MessageSource messageSource;
 
+    @ApiOperation(value = "View a list of available tasks", response = List.class)
     @GetMapping
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
     }
 
+    @ApiOperation(value = "Get a task by Id")
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
         try {
@@ -41,17 +46,20 @@ public class TaskController {
     }
 
 
+    @ApiOperation(value = "Create a new task")
     @PostMapping
     public Task createTask(@Valid @RequestBody Task task) {
         return taskService.createTask(task);
     }
 
+    @ApiOperation(value = "Update an existing task")
     @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable long id, @RequestBody Task taskDetails) {
+    public ResponseEntity<Task> updateTask(@PathVariable long id, @Valid @RequestBody Task taskDetails) {
         final Task updatedTask = taskService.updateTask(id, taskDetails);
         return ResponseEntity.ok(updatedTask);
     }
 
+    @ApiOperation(value = "Delete a task")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
 
@@ -59,7 +67,7 @@ public class TaskController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping
+    @GetMapping("/paged")
     public ResponseEntity<Page<Task>> getTasks(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
